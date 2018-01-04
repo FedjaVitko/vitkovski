590484c945fa585be64dceaf10b84b82e99a3072
TheoVitkovskiy <fvitkovski@mail.de>
2018-01-04 23:41:54 +0100

    Add build configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const webpack = require('gulp-webpack');
+const imagemin = require('gulp-imagemin');
 
 const webpackConfig = require('./webpack.config.js');
 
@@ -10,30 +11,57 @@ sass = require('gulp-sass');
 gulp.task('watch', ['sass', 'js'], function() {
 
     browserSync.init({
-        server: '.'
+        server: './app'
     });
 
-    gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js', ['js'], browserSync.reload);
+    gulp.watch('app/scss/**/*.scss', ['sass'], browserSync.reload);
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('app/js/**/*.js', ['js'], browserSync.reload);
 
 });
 
 // Compile Sass into CSS & inject into browsers
 gulp.task('sass', function() {
-    return gulp.src('./scss/**/*.scss')
+    return gulp.src('app/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
-    return gulp.src('js/index.js')
+    return gulp.src('app/js/index.js')
         .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('dist/'))
+        .pipe(gulp.dest('app/'))
         .pipe(browserSync.stream());
   });
 
+  
+  // DIST
+  
+gulp.task('html', function(){
+    gulp.src('app/*.html')
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('css-dist', function () {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js-dist', function () {
+    return gulp.src('app/js/index.js')
+        .pipe(webpack(webpackConfig))
+        .pipe(gulp.dest('./dist/'))
+})
+
+// minify images
+gulp.task('images', function(){
+    return gulp.src('app/img/**/*.*')
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('build', ['html', 'css-dist', 'js-dist', 'images']);
 
 // default will also watch
 gulp.task('default', ['watch']);
